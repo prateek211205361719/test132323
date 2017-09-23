7f0e9d8c5890a213d7016499fe268657020d9bb7
prateek211205361719 <prateek@ceptes.com>
2017-09-24 00:01:06 +0530

    33

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ app.use(passport.session());
 authRoute(app);
 //billing(app);
 
-if(process.env.NODE_ENV === 'PRODUCTION'){
+if(process.env.NODE_ENV === 'production'){
     app.use(express.static('client/build'));
     const path = require('path');
     app.get('*', (req, res) => {
